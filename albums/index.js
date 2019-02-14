28edf73be671e5c93110be1cc3ec1034cfd3a788
hnorza <norzagaray@me.com>
2019-02-13 17:31:43 -0800

    added scaffold for app

---
+++ albums/index.js
@@ -0,0 +1,10 @@
+//We need to import the library to help create component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+
+//create component
+
+
+
+//render component on device
